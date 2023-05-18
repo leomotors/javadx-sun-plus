20c71a057dc308a8b078c6578dcfea0d13826984
Thanakrit Wongasa <gain_thanakrit@kkumail.com>
2023-05-19 06:12:15 +0700

    feat: add select soundFX

--- src/controller/PartnerSelectionController.java
+++ src/controller/PartnerSelectionController.java
@@ -1,9 +1,12 @@
 package controller;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.scene.media.AudioClip;
+import javafx.util.Duration;
 import router.AppPage;
 import router.Router;
 import store.DataManager;
@@ -17,22 +20,32 @@ public class PartnerSelectionController {
     @FXML
     private Button SelectJavaButton;
 
+    private AudioClip selectFx = new AudioClip(ClassLoader
+            .getSystemResource("sounds/fx/SELECT.mp4")
+            .toString());
+
     @FXML
     private void SelectCpp() {
         DataManager.getInstance().set(Setting.PARTNER, "CPP");
+        selectFx.play();
         AudioClip Cpp = new AudioClip(ClassLoader
                 .getSystemResource("sounds/partner/CPP_SELECT.wav")
                 .toString());
-        Cpp.play();
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(0.5), event -> Cpp.play()));
+        timeline.play();
     }
 
     @FXML
     private void SelectJava() {
         DataManager.getInstance().set(Setting.PARTNER, "JAVA");
+        selectFx.play();
         AudioClip Java = new AudioClip(ClassLoader
                 .getSystemResource("sounds/partner/JAVA_SELECT.wav")
                 .toString());
-        Java.play();
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(0.5), event -> Java.play()));
+        timeline.play();
     }
 
     @FXML
