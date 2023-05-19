02e5012fa5818ccae7dceaae5e2f6f40c57d4e55
Thanakrit Wongasa <gain_thanakrit@kkumail.com>
2023-05-20 02:01:59 +0700

    refactor: better lane highlight

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -61,8 +61,8 @@ public class GameController {
 
     public void start() {
         for (int i = 0; i < Config.LANE_COUNT; i++) {
-            drawLane(i);
             active.add(false);
+            drawLane(i);
         }
     }
 
@@ -71,10 +71,12 @@ public class GameController {
         double x1 = Config.LANE_BOTTOM_WIDTH * laneNumber;
         double buffer = (WIDTH - Config.LANE_TOP_WIDTH * Config.LANE_COUNT) / 2;
         double x2 = buffer + Config.LANE_TOP_WIDTH * laneNumber;
-        gc.setFill(Color.BLACK);
+        if (active.get(laneNumber))
+            gc.setFill(Color.web("#383f47"));
+        else
+            gc.setFill(Color.BLACK);
         gc.setStroke(Color.WHITE);
         gc.setLineWidth(1.0);
-        // gc.setGlobalAlpha(0.8);
         gc.beginPath();
         gc.moveTo(x1, Config.LANE_HEIGHT);
         gc.lineTo(x2, 0);
@@ -83,139 +85,104 @@ public class GameController {
         gc.closePath();
         gc.fill();
         gc.stroke();
-    }
-
-    private void activateLane(int laneNumber) {
-        GraphicsContext gc = PlayArea.getGraphicsContext2D();
-        double x1 = Config.LANE_BOTTOM_WIDTH * laneNumber;
-        double buffer = (WIDTH - Config.LANE_TOP_WIDTH * Config.LANE_COUNT) / 2;
-        double x2 = buffer + Config.LANE_TOP_WIDTH * laneNumber;
-        gc.setFill(Color.web("#383f47"));
-        gc.setStroke(Color.WHITE);
-        gc.setLineWidth(1.0);
-        // gc.setGlobalAlpha(0.8);
-        gc.beginPath();
-        gc.moveTo(x1, Config.LANE_HEIGHT);
-        gc.lineTo(x2, 0);
-        gc.lineTo(x2 + Config.LANE_TOP_WIDTH, 0);
-        gc.lineTo(x1 + Config.LANE_BOTTOM_WIDTH, Config.LANE_HEIGHT);
-        gc.closePath();
-        gc.fill();
-        gc.stroke();
-    }
-
-    @FXML
-    public void handleKeyRelease(KeyEvent e) {
-        switch (e.getCode()) {
-            case A:
-                drawLane(0);
-                active.set(0, false);
-                break;
-            case S:
-                drawLane(1);
-                active.set(1, false);
-                break;
-            case D:
-                drawLane(2);
-                active.set(2, false);
-                break;
-            case F:
-                drawLane(3);
-                active.set(3, false);
-                break;
-            case G:
-                drawLane(4);
-                active.set(4, false);
-                break;
-            case H:
-                drawLane(5);
-                active.set(5, false);
-                break;
-            case J:
-                drawLane(6);
-                active.set(6, false);
-                break;
-            case K:
-                drawLane(7);
-                active.set(7, false);
-                break;
-            case L:
-                drawLane(8);
-                active.set(8, false);
-                break;
-            case SEMICOLON:
-                drawLane(9);
-                active.set(9, false);
-                break;
-            case QUOTE:
-                drawLane(10);
-                active.set(10, false);
-                break;
-            case ENTER:
-                drawLane(11);
-                active.set(11, false);
-                break;
-            default:
-                break;
-        }
-        e.consume();
     }
 
     @FXML
     public void handleKeyPress(KeyEvent e) {
-        System.out.println(e.getCode());
+        int laneNum = -1;
         switch (e.getCode()) {
             case A:
-                activateLane(0);
-                active.set(0, true);
+                laneNum = 0;
                 break;
             case S:
-                activateLane(1);
-                active.set(1, true);
+                laneNum = 1;
                 break;
             case D:
-                activateLane(2);
-                active.set(2, true);
+                laneNum = 2;
                 break;
             case F:
-                activateLane(3);
-                active.set(3, true);
+                laneNum = 3;
                 break;
             case G:
-                activateLane(4);
-                active.set(4, true);
+                laneNum = 4;
                 break;
             case H:
-                activateLane(5);
-                active.set(5, true);
+                laneNum = 5;
                 break;
             case J:
-                activateLane(6);
-                active.set(6, true);
+                laneNum = 6;
                 break;
             case K:
-                activateLane(7);
-                active.set(7, true);
+                laneNum = 7;
                 break;
             case L:
-                activateLane(8);
-                active.set(8, true);
+                laneNum = 8;
                 break;
             case SEMICOLON:
-                activateLane(9);
-                active.set(9, true);
+                laneNum = 9;
                 break;
             case QUOTE:
-                activateLane(10);
-                active.set(10, true);
+                laneNum = 10;
                 break;
             case ENTER:
-                activateLane(11);
-                active.set(11, true);
+                laneNum = 11;
                 break;
             default:
                 break;
         }
-        e.consume();
+        if (laneNum != -1) {
+            active.set(laneNum, true);
+            drawLane(laneNum);
+        }
     }
+
+    @FXML
+    public void handleKeyRelease(KeyEvent e) {
+        int laneNum = -1;
+        switch (e.getCode()) {
+            case A:
+                laneNum = 0;
+                break;
+            case S:
+                laneNum = 1;
+                break;
+            case D:
+                laneNum = 2;
+                break;
+            case F:
+                laneNum = 3;
+                break;
+            case G:
+                laneNum = 4;
+                break;
+            case H:
+                laneNum = 5;
+                break;
+            case J:
+                laneNum = 6;
+                break;
+            case K:
+                laneNum = 7;
+                break;
+            case L:
+                laneNum = 8;
+                break;
+            case SEMICOLON:
+                laneNum = 9;
+                break;
+            case QUOTE:
+                laneNum = 10;
+                break;
+            case ENTER:
+                laneNum = 11;
+                break;
+            default:
+                break;
+        }
+        if (laneNum != -1) {
+            active.set(laneNum, false);
+            drawLane(laneNum);
+        }
+    }
+
 }
