9cabb809132b95bb2d2ff1e604a4c61f9fc30139
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2023-05-21 19:45:42 +0700

    feat: idk

--- src/constant/JudgementWindow.java
+++ src/constant/JudgementWindow.java
@@ -7,4 +7,8 @@ public final class JudgementWindow {
     public static final int CRITICAL_PERFECT = 42;
     public static final int PERFECT = 69;
     public static final int GOOD = 112;
+
+    public static final int HOLD_PLATINUM_CRITICAL_PEFECT = 50;
+    public static final int HOLD_PERFECT = 100;
+    public static final int HOLD_GOOD = 150;
 }
